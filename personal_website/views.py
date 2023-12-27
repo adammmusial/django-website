250bc25a81934b9aceae0416557c07ddbc5eced0
adammmusial <adammusial@MacBook-Pro-Adam.local>
2023-12-27 22:05:18 +0800

    feat: add types for functions

--- personal_website/views.py
+++ personal_website/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-def index(request):
+def index(request: HttpRequest) -> HttpRequest:
     print(request.user)
     return render(request,'index.html')
 
-def about(request):
+def about(request HttpRequest) -> HttpRequest:
     return render(request,'about.html')
 
-def contact(request):
+def contact(request HttpRequest) -> HttpRequest:
     return render(request,'contact.html')
 
 
