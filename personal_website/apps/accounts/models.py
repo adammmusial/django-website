edcedb63f6d37ffe3303b5e9777c14bb9c27e72c
adammmusial <adammusial@MacBook-Pro-Adam.local>
2023-12-28 15:58:34 +0800

    feat: create ManyToMany relationship for UserInterest class with UserProfile class

--- personal_website/apps/accounts/models.py
+++ personal_website/apps/accounts/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class UserInterest(models.Model):
     name = models.CharField(max_length=64, unique=True)
     normalized_name = models.CharField(max_length=64, unique=True)
@@ -15,15 +16,22 @@ class UserPersona(models.Model):
     name = models.CharField(max_length=64, unique=True)
     normalized_name = models.CharField(max_length=64, unique=True)
     description = models.CharField(max_length=200)
-    
 
     def __str__(self):
         return self.name
 
 
 class UserProfile(models.Model):
+    #owner
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    
+    #settings
     is_full_name_displayed = models.BooleanField(default=True)
+
+    #details
     bio = models.CharField(max_length=500, blank=True, null=True)
     website = models.URLField(max_length=200, blank=True, null=True)
-    persona = models.ForeignKey(UserPersona, on_delete=models.SET_NULL, blank=True, null=True)
+    persona = models.ForeignKey(
+        UserPersona, on_delete=models.SET_NULL, blank=True, null=True
+    )
+    interest = models.ManyToManyField(UserInterest, blank=True)
