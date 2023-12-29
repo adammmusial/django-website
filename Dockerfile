855c9cd0c9ec7ff1dfbfd403ce909b46f1210990
adammmusial <adammusial@MacBook-Pro-Adam.local>
2023-12-29 12:11:38 +0800

    fix: add 0.0.0.0 to allowed hosts and implement features to docker file

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,17 @@ FROM python:3.11.7-slim as production
 ENV PYTHONUNBUFFERED=1
 WORKDIR /app/
 
+RUN apt-get update && \
+    apt-get install -y \
+    bash \
+    build-essential \
+    gcc \
+    libffi-dev \
+    musl-dev \
+    openssl \
+    postgresql \
+    libpq-dev
+
 COPY requirements/prod.txt ./requirements/prod.txt
 RUN pip install -r ./requirements/prod.txt
 
@@ -16,6 +27,6 @@ EXPOSE 8000
 FROM production as development
 
 COPY requirements/dev.txt ./requirements/dev.txt
-RUN pip install -r /requirements/dev.txt
+RUN pip install -r requirements/dev.txt
 
 COPY . . 
--- personal_website/settings.py
+++ personal_website/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-!!-b!(9!lz_-)-if61i5$r*k51bbtipo1pgbrao3rjlyp3zsri
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','0.0.0.0']
 
 
 # Application definition
@@ -77,10 +77,19 @@ WSGI_APPLICATION = "personal_website.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+POSTGRES_HOST = os.environ.get('POSTGRES_HOST',default="")
+POSTGRES_DB = os.environ.get('POSTGRES_DB',default="")
+POSTGRES_USER = os.environ.get('POSTGRES_USER',default="")
+POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD',default="")
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": POSTGRES_DB,
+        "USER": POSTGRES_USER,
+        "PASSWORD": POSTGRES_PASSWORD,
+        "HOST": POSTGRES_HOST,
+        "PORT": 5432,
     }
 }
 
