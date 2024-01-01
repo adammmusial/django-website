f62b087ac78b1b364158e918d1aec506a93508ea
adammmusial <adammusial@MacBook-Pro-Adam.local>
2024-01-01 13:06:06 +0800

    feat: change html and views.py code

--- personal_website/apps/contact/views.py
+++ personal_website/apps/contact/views.py
@@ -7,12 +7,15 @@ from .forms import ContactForm
 def contact(request: HttpRequest) -> HttpResponse:
     if request.method == "GET":
         form = ContactForm()
-        return render(
-            request, "contact.html", {"form": form}
-        )  # Return the rendered response
-    else:
-        raise NotImplementedError
 
+    elif request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            pass
+    else: 
+        raise NotImplementedError
+    
+    return render(request, "contact.html", {"form": form})
 
 # def contact_form(request: HttpRequest) -> HttpResponse:
 #     return render(request, 'contact.html')  # Return the rendered response
