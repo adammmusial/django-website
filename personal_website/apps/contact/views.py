99aac1784649d7210a58366ff94eefd8e8bcde87
adammmusial <adammusial@MacBook-Pro-Adam.local>
2024-01-01 13:30:35 +0800

    feat: implement success and error messages when trying to send email

--- personal_website/apps/contact/views.py
+++ personal_website/apps/contact/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
-
+from django.core.mail import send_mail
 from .forms import ContactForm
 
 
@@ -11,7 +11,11 @@ def contact(request: HttpRequest) -> HttpResponse:
     elif request.method == "POST":
         form = ContactForm(request.POST)
         if form.is_valid():
-            pass
+            name = form.cleaned_data["name"]
+            email = form.cleaned_data["email"]
+            message = form.cleaned_data["message"]
+            send_mail(f"{name} sent an email", message, mail, [settings.DEFAULT_FROM_EMAIL])
+            return render(request, "contact.html", {"form": form}, "success": True) 
     else: 
         raise NotImplementedError
     
